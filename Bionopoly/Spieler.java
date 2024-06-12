266eca94c0be619a40538606ee5e76f0690daa17
SinaPf23 <sina3209m@gmail.com>
2024-06-12 15:33:40 +0200

    Update Spieler.java

--- Bionopoly/Spieler.java
+++ Bionopoly/Spieler.java
@@ -2,18 +2,25 @@ package bionopoly;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Spieler {
     private String name;
-    private Währung kontostand;
-    private List<Spielfeld> besitz;
+    private int kontostand;
+    private List<Feld> besitz;
     private int position;
+    private int intelligenz;
+    private boolean pleite;
 
-    public Spieler(String name, int startgeld) {
+    public Spieler(String name, int startgeld, int startIntelligenz) {
         this.name = name;
-        this.kontostand = new Währung(startgeld);
+        Währung währung = new Währung();
+        währung.Startgeld(startgeld);
+        this.kontostand = währung.getStartgeld();
         this.besitz = new ArrayList<>();
         this.position = 0;
+        this.intelligenz = startIntelligenz;
+        this.pleite = false;
     }
 
     public String getName() {
@@ -28,18 +35,86 @@ public class Spieler {
         this.position = position;
     }
 
-    public Währung getKontostand() {
-        return kontostand;
+    public int getIntelligenz() {
+        return intelligenz;
     }
-
-    public List<Spielfeld> getBesitz() {
+    public void setIntelligenz(int intelligenz) {
+        this.intelligenz = intelligenz;
+	}
+    
+    
+    public List<Feld> getBesitz() {
         return besitz;
     }
+    
+    
+    public boolean isPleite() {
+        return pleite;
+    }
 
-    public void kaufeFeld(Spielfeld feld) {
-        if (feld.istVerfuegbar() && kontostand.subtrahiere(feld.getPreis())) {
-            feld.setBesitzer(this);
-            besitz.add(feld);
+    public void setPleite(boolean pleite) {
+        this.pleite = pleite;
+    }
+
+ //Methode zur Überprüfung, ob ein Feld einen Besitzer hat
+    public boolean hatBesitzer(Feld feld) {
+        return feld.getBesitzer() != null;
+    }
+
+    //Methode, um ein Feld zu kaufen, wenn es noch keinen Besitzer hat
+    public void feldKaufen(Feld feld, List<Feld> unkaufbareFelder) {
+        if (!hatBesitzer(feld) && !unkaufbareFelder.contains(feld)) {
+            System.out.println("Möchtest du " + feld.getPreis() + " Intelligenz in das Modul " + feld.getName() + " investieren? (Ja/Nein)");
+            Scanner scanner = new Scanner(System.in);
+            String antwort = scanner.nextLine();
+            
+            if (antwort.equalsIgnoreCase("ja")) {
+                if (intelligenz >= feld.getPreis()) {
+                    setIntelligenz(getIntelligenz() - feld.getPreis());
+                    feld.setBesitzer(this);
+                    besitz.add(feld); //Hier das Feld zur Liste hinzufügen
+                    System.out.println(name + " hat " + feld.getName() + " für " + feld.getPreis() + " Intelligenz erhalten.");
+                } else {
+                    System.out.println(name + " hat nicht genug Intelligenz, um " + feld.getName() + " zu belegen.");
+                }
+            } else {
+                System.out.println("Du hast entschieden, " + feld.getName() + " zu schieben.");
+            }
+        } else {
+            System.out.println(feld.getName() + " kann nicht erhalten werden.");
         }
     }
+
+    //Methode, um die Miete für ein Feld zu zahlen
+    public void mieteZahlen(Feld feld, Spielfigur spieler) {
+        if (hatBesitzer(feld) && !feld.getBesitzer().equals(spieler)) {
+            int miete = feld.getMiete();
+            Spieler besitzer = feld.getBesitzer();
+            if (spieler.getIntelligenz() >= miete) {
+                spieler.setIntelligenz(spieler.getIntelligenz() - miete);
+                besitzer.setIntelligenz(besitzer.getIntelligenz() + miete);
+                System.out.println(spieler.getName() + " hat " + miete + " Intelligenz an " + besitzer.getName() + " für Nachhilfe in dem Modul " + feld.getName() + " gegeben.");
+            } 
+            else {
+                System.out.println(spieler.getName() + " hat nicht genug Intelligenz, um die Nachilfe für das Modul " + feld.getName() + " zu verstehen. ");
+            }
+            
+            //Behandlung spezifischer Ereignisse für bestimmte Felder
+            if (feld.getName().equalsIgnoreCase("O-Woche")) {
+                spieler.setIntelligenz(spieler.getIntelligenz() - 200);
+                System.out.println(spieler.getName() + " hat 200 Intelligenz aufgrund von zu hohem Alkoholkonsum verloren.");
+            } 
+            else if (feld.getName().equalsIgnoreCase("Bachelorabschlussparty")) {
+                spieler.setIntelligenz(spieler.getIntelligenz() - 200);
+                System.out.println(spieler.getName() + " hat 200 Intelligenz aufgrund zu starker Feierns auf der Bachelorabschlussparty verloren.");
+            } 
+        }
+    }
+    
+ //Methode zur Überprüfung, ob ein Spieler pleite ist
+    public void spielerPleite(Spielfigur spieler) {
+        System.out.println(spieler.getName() + " hat all seine Intelligenz verloren und muss das Bionik Studium schmeißen. ");
+        spieler.setPleite(true);
+    }
+    
 }
