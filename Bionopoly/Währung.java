599bf9cfac1276fb0ea3eeefce5892d6cca0cf48
mathildefreitag <168734519+mathildefreitag@users.noreply.github.com>
2024-06-13 17:09:48 +0200

    Update Währung.java

--- Bionopoly/Währung.java
+++ Bionopoly/Währung.java
@@ -1,45 +1,84 @@
 package bionopoly;
 
-import java.util.*; //Package damit man später Zufallszahlen bekommt
+public class Währung {
+    
+    private Spielfeld[][] feld;
+    private Spielfigur[] spieler;
+    private int startgeld;
+    private int überStart;
+    private int transaktion;
 
-public class Würfel {
-	private int augensumme;
-	private int würfel1;
-	private int würfel2;
-	
-	public void würfel() {
-		
-		Random rand = new Random(); //für Zufallszahlen
-
-		{
-		    würfel1 = rand.nextInt(6) + 1; //Zufallszahl für würfel1 erstellen (initialisieren und deklarieren)
-			würfel2 = rand.nextInt(6) + 1; //Zufallszahl für würfel2 erstellen (initialisieren und deklarieren)
-			this.augensumme = würfel1 + würfel2;//Augensumme der beiden Würfel addieren
-			
-			//(Ausgabe der Werte)
-			System.out.print("Würfel 1: " + würfel1);
-			System.out.println();
-			System.out.print("Würfel 2: " + würfel2);
-			System.out.println();
-			System.out.print("Augensumme: " + augensumme + "\n");
-			
-		}
-	}
-	//Methode zum Abrufen der Augensumme
-    public int getAugensumme() {
-        return augensumme;
+    
+    public Währung() {
+    	
     }
-    //Methode zum Abrufen der Augenzahl des ersten Würfels
-    public int getWürfel1() {
-        return würfel1;
+    //Konstruktor
+    public Währung(Spielfeld[][] feld, Spielfigur[] spieler, int überStart, int transaktion) {
+        this.feld = feld;
+        this.spieler = spieler;
+        this.überStart = überStart;
+        this.transaktion = transaktion;
+    }
+    
+    public void Startgeld(int startgeld) {
+    	this.startgeld = startgeld;
     }
 
-    //Methode zum Abrufen der Augenzahl des zweiten Würfels
-    public int getWürfel2() {
-        return würfel2;
+  
+    public Spielfeld[][] getFeld() {
+        return feld;
     }
-    //Methode zur Erkennung eines Paschs
-    public boolean istPasch() {
-        return würfel1 == würfel2;
+
+    public void setFeld(Spielfeld[][] feld) {
+        this.feld = feld;
     }
+
+  
+    public Spielfigur[] getSpieler() {
+        return spieler;
+    }
+
+    public void setSpieler(Spielfigur[] spieler) {
+        this.spieler = spieler;
+    }
+
+    
+    public int getStartgeld() {
+        return startgeld;
+    }
+
+    public void setStartgeld(int startgeld) {
+        this.startgeld = startgeld;
+    }
+
+   
+    public int getStartfeldgeld() {
+        return überStart;
+    }
+
+    public void setStartfeldgeld(int überStart) {
+        this.überStart = überStart;
+    }
+
+
+    public int getTransaktion() {
+        return transaktion;
+    }
+
+    public void setTransaktion(int transaktion) {
+        this.transaktion = transaktion;
+    }
+
+    //Methode zur Initialisierung der Spieler mit StartIntelligenz
+    public void initialisiereSpieler() {
+        for (Spielfigur s : spieler) {
+            s.setIntelligenz(1500); // Jeder Spieler erhält 1500 Währung als StartIntelligenz
+        }
+    }
+
+    //Methode, die einem Spieler 200 Währung gibt, wenn er über das Startfeld kommt
+    public void überStart(Spielfigur spieler) {
+        spieler.setIntelligenz(spieler.getIntelligenz() + 200); // Spieler erhält 200 Währung
+    }
+
 }
