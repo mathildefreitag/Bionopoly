54879f28c20c945d5fc25be50053b3726ee2c9ef
mathildefreitag <168734519+mathildefreitag@users.noreply.github.com>
2024-06-13 16:58:35 +0200

    Update Spielfigur.java

--- Bionopoly/Spielfigur.java
+++ Bionopoly/Spielfigur.java
@@ -1,140 +1,91 @@
-package bionopoly;
+package gui;
 
-import java.util.ArrayList;
-import java.util.List;
+import bionopoly.Spielfigur;
+import bionopoly.Würfel;
+import bionopoly.Spielfeld;
+import bionopoly.Feld;
 
-public class Spielfigur {
-    // Name deklarieren
-    private String name;
-    private int intelligenz;
-    private boolean pleite;
-    private Feld aktuellesFeld;
-    private Spielfeld spielfeld;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
-    // Konstruktor zum Initialisieren des Namens
-    public Spielfigur(String name, Feld startFeld, Spielfeld spielfeld) {
-        this.name = name;
-        this.intelligenz = 1500; // Startkapital
-        this.pleite = false;
-        this.aktuellesFeld = startFeld;
-        this.spielfeld = spielfeld; // Spielfeld zuweisen
+public class SpielfigurGui extends JFrame {
+    private Map<String, JLabel> spielfigurenLabels;
+    private JPanel spielfeld;
+    private Spielfeld feldAmOrt;
+
+    public SpielfigurGui() {
+        setTitle("Bionopoly");
+        setSize(800, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        spielfeld = new JPanel();
+        spielfeld.setLayout(null);
+        int startfeld = 0;
+
         
-    }
+        spielfeld = new Spielfeld(50, 50, 612, 612);
 
-    // Methode zum Aufrufen des Namens
-    public String getName() {
-        return name;
-    }
-    
 
-    // Methode um auf augensumme Zugriff zu haben und Spielfigur basierend auf der Augensumme zu verschieben
-   /* public void felderVorrücken(Würfel würfel) {
-        int augensumme = würfel.getAugensumme();
-        System.out.println("Die Spielfigur " + name + " rückt um " + augensumme + " Felder vor.");
-    }
-	*/
-    public void felderVorrücken(Spielfeld spielfeld, int augensumme) {
-        int neuesFeldIndex = (aktuellesFeld.getIndex() + augensumme) % spielfeld.getAlleFelder().size();
-        Feld neuesFeld = spielfeld.feldAmOrt(neuesFeldIndex);
-        setAktuellesFeld(neuesFeld);
-        System.out.println(name + " ist auf " + neuesFeld.getName() + " gelandet.");
-    }
-    
-    public static void main(String[] args) {
-    	 // Initialisiere das Spielfeld und die Spielfiguren
-        Spielfeld spielfeld = new Spielfeld(0, 0, 800, 800);
-        /*List<Spielfigur> spielfiguren = new ArrayList<>();
-        initSpielfiguren(spielfiguren, spielfeld);
-		*/
-        List<Spielfigur> spielfiguren = initSpielfiguren(spielfeld);
-        /*// Setze die Anfangspositionen der Spielfiguren
-        for (Spielfigur figur : spielfiguren) {
-            figur.setAktuellesFeld(spielfeld.feldAmOrt(0));
-        }
-		*/
-        // Beispielhafte Verwendung der Spielfigur und Würfeln
-        Spielfigur figur = spielfiguren.get(0); // Erste Spielfigur
-        Würfel würfel = new Würfel();
-        würfel.würfel();
-        int augensumme = würfel.getAugensumme();
-        figur.felderVorrücken(spielfeld, augensumme);
-    }
-    // Initialisiere die Spielfiguren auf dem Startfeld
-    List<Spielfigur> spielfiguren = new ArrayList<>();
-    Feld startFeld = spielfeld.feldAmOrt(00);
-    //void initSpielfiguren(spielfiguren, startFeld);
-
-    // Initialisiert die 6 verschiedenen Spielfiguren
-    /*private static void initSpielfiguren(List<Spielfigur> spielfiguren, Spielfeld spielfeld) {
-        Feld startFeld = spielfeld.feldAmOrt(00);
-    	spielfiguren.add(new Spielfigur("Regenwurm"));
-        spielfiguren.add(new Spielfigur("Paramecium"));
-        spielfiguren.add(new Spielfigur("Heuschrecke"));
-        spielfiguren.add(new Spielfigur("Fisch"));
-        spielfiguren.add(new Spielfigur("Seestern"));
-        spielfiguren.add(new Spielfigur("Schwein"));
-    }
-	*/
-    private static List<Spielfigur> initSpielfiguren(Spielfeld spielfeld) {
-        List<Spielfigur> spielfiguren = new ArrayList<>();
-        spielfiguren.add(new Spielfigur("Regenwurm", spielfeld.feldAmOrt(0), spielfeld));
-        spielfiguren.add(new Spielfigur("Paramecium", spielfeld.feldAmOrt(0), spielfeld));
-        spielfiguren.add(new Spielfigur("Heuschrecke", spielfeld.feldAmOrt(0), spielfeld));
-        spielfiguren.add(new Spielfigur("Fisch", spielfeld.feldAmOrt(0), spielfeld));
-        spielfiguren.add(new Spielfigur("Seestern", spielfeld.feldAmOrt(0), spielfeld));
-        spielfiguren.add(new Spielfigur("Schwein", spielfeld.feldAmOrt(0), spielfeld));
-        return spielfiguren;
-    }
-    
- // Methode zum Anzeigen aller Spielfiguren
-    private static void zeigeSpielfiguren(List<Spielfigur> spielfiguren) {
-        for (int i = 0; i < spielfiguren.size(); i++) {
-            System.out.println((i + 1) + ". " + spielfiguren.get(i).getName());
-        }
-    }
-    // Methode zum Verschieben einer Spielfigur von einer Position zu einer anderen
-   
-    ////Spielfigur aktuellerSpieler = spielfiguren.get(0);
-    ////Würfel würfel = new Würfel();
-
-   /* int paschCount = 0
-
-    for (int i = 0; i < 3; i++) { // Simuliert drei Würfe
-        würfel.würfel();
-
-        if (würfel.istPasch()) {
-            paschCount++;
-            if (paschCount == 3) {
-                aktuellerSpieler.zieheNachPrüfung();
-                break;
+        String imagePath = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\fisch.png";
+        String imagePath1 = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\regenwurm.png";
+        String imagePath2 = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\paramecium.png";
+        String imagePath3 = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\schwein.png";
+        String imagePath4 = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\heuschrecke.png";
+        String imagePath5 = "C:\\Users\\freit\\eclipse-workspace\\Bionopoly\\src\\gui\\seestern.png";
+        
+        JButton würfelnButton = new JButton("Würfeln");
+        würfelnButton.setBounds(350, 700, 100, 30);
+        würfelnButton.addActionListener(new ActionListener() {
+      
+            public void actionPerformed(ActionEvent e) {
+                Würfel würfel = new Würfel();
+                würfel.würfel();
+                int augensumme = würfel.getAugensumme();
+                System.out.println("Gewürfelte Augensumme: " + augensumme);
             }
-        } else {
-            paschCount = 0;
-        }
+        });
 
-        ////aktuellerSpieler.felderVorrücken(würfel);
-        */
-        public int getIntelligenz() {
-            return intelligenz;
-        }
+        spielfeld.add(würfelnButton);
+        add(spielfeld);
+    }
 
-        public void setIntelligenz(int intelligenz) {
-            this.intelligenz = intelligenz;
-        }
-
-        public boolean isPleite() {
-            return pleite;
-        }
-
-        public void setPleite(boolean pleite) {
-            this.pleite = pleite;
+    private void addSpielfigur(Spielfigur spielfigur, String bildDatei) {
+        //Überprüfen, ob die Ressource vorhanden ist
+        java.net.URL bildURL = getClass().getResource("/" + bildDatei);
+        if (bildURL == null) {
+            System.out.println("Bilddatei nicht gefunden: " + bildDatei);
+            return;
         }
         
-        public Feld getAktuellesFeld() {
-            return aktuellesFeld;
-        }
+        ImageIcon icon = new ImageIcon(bildURL);
+        JLabel label = new JLabel(icon);
+        label.setBounds(50, 50, icon.getIconWidth(), icon.getIconHeight());
+        spielfeld.feldAmOrt(spielfigur.getPosition()).add(label); //Füge das Label dem richtigen Feld hinzu
+        spielfeld.add(label);
+        spielfigurenLabels.put(spielfigur.getName(), label);
+    }
 
-        public void setAktuellesFeld(Feld aktuellesFeld) {
-            this.aktuellesFeld = aktuellesFeld;
+    private void verschiebeSpielfigur(String name, int x, int y) {
+        JLabel label = spielfigurenLabels.get(name);
+        if (label != null) {
+            label.setLocation(x, y);
+        } 
+        else {
+            System.out.println("Spielfigur mit Namen " + name + " nicht gefunden.");
         }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+           
+            public void run() {
+                new SpielfigurGui().setVisible(true);
+            }
+        });
+    }
 }
