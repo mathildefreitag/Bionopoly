9f495e69dc1c031f2942fda7099e33fd3876f002
Theresa <168730143+TheresaRiess@users.noreply.github.com>
2024-06-13 15:07:23 +0200

    Add files via upload

--- Bionopoly/StartbildschirmGui.java
+++ Bionopoly/StartbildschirmGui.java
@@ -13,8 +13,13 @@ public class StartbildschirmGui extends JPanel {
     private SpielbrettGui2 spielbrettGui; // Referenz auf das Spielbrett
 
     public StartbildschirmGui() {
-        setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        setLayout(new GridBagLayout()); // Verwendung eines GridBagLayouts für präzisere Platzierung
         setBackground(Color.LIGHT_GRAY);
+
+        // Begrüßungstext
+        JLabel willkommenLabel = new JLabel("<html><div style='font-size: 24px; text-align: center;'>Willkommen zu Bionopoly</div><br>" +
+                                            "<div style='font-size: 14px; text-align: center;'>Bitte wählen Sie die gewünschte Spieleranzahl aus</div></html>");
+        willkommenLabel.setPreferredSize(new Dimension(400, 100)); // Größe anpassen
 
         JLabel spielerAnzahlLabel = new JLabel("Anzahl der Spieler:");
         spielerAnzahlComboBox = new JComboBox<>(new String[]{"2 Spieler", "3 Spieler", "4 Spieler", "5 Spieler", "6 Spieler"});
@@ -53,10 +58,30 @@ public class StartbildschirmGui extends JPanel {
             }
         });
 
-        add(spielerAnzahlLabel);
-        add(spielerAnzahlComboBox);
-        add(startButton);
-        add(beendenButton);
+        // Verwendung von GridBagConstraints, um Komponenten zentriert anzuordnen
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(50, 0, 20, 0); // Abstände anpassen
+        add(willkommenLabel, gbc);
+
+        gbc.gridy = 1;
+        gbc.insets = new Insets(10, 0, 10, 0); // Abstände anpassen
+        add(spielerAnzahlLabel, gbc);
+
+        gbc.gridx = 1;
+        add(spielerAnzahlComboBox, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER; // Zentrierung der Buttons
+        gbc.insets = new Insets(20, 0, 0, 0); // Abstände anpassen
+        add(startButton, gbc);
+
+        gbc.gridy = 3;
+        gbc.insets = new Insets(10, 0, 50, 0); // Abstände anpassen
+        add(beendenButton, gbc);
     }
 
     public static void main(String[] args) {
@@ -65,8 +90,17 @@ public class StartbildschirmGui extends JPanel {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setResizable(false);
             frame.getContentPane().add(new StartbildschirmGui());
-            frame.pack();
-            frame.setLocationRelativeTo(null);
+
+            // Setzen des Vollbildmodus
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setUndecorated(true); // Rahmen entfernen für Vollbild
+
+            // Fenster zentrieren
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int centerX = screenSize.width / 2;
+            int centerY = screenSize.height / 2;
+            frame.setLocation(centerX - frame.getWidth() / 2, centerY - frame.getHeight() / 2);
+
             frame.setVisible(true);
         });
     }
