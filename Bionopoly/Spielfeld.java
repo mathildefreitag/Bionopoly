64bc0048cd5572054dfdbec9bffcd3e3b7b44245
Theresa <168730143+TheresaRiess@users.noreply.github.com>
2024-07-18 16:50:42 +0200

    Update Spielfeld.java

--- Bionopoly/Spielfeld.java
+++ Bionopoly/Spielfeld.java
@@ -12,26 +12,32 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 public class Spielfeld extends JPanel {
+	
     private static final long serialVersionUID = 1L;
 
     private static ArrayList<Feld> alleFelder = new ArrayList<>();
     private ArrayList<Spielfigur> spielerListe = new ArrayList<>();
     private List<Feld> unkaufbareFelder = new ArrayList<>();
-    private static int anzahlFelder = 40;
+    
     private boolean canRollDice = true;
-    private static int currentPlayerIndex = 0;
-    private Spielfigur intelligenz;
-    static int miete;
-    static String farbe;
     static boolean besitzer;
+    
+    private static int currentPlayerIndex = 0;
+    static int miete;
+
+    static String farbe;
     static String besucher;
     static String feldname;
-    private int anzahlPasche;
     
-
+    public Spielfeld(int xKoord, int yKoord, int breite, int hoehe) {
+        setBorder(new LineBorder(new Color(0, 0, 0)));
+        setBounds(xKoord, yKoord, breite, hoehe);
+        this.setLayout(null);
+        initialisierungModule();
+    }
+ 
     public ArrayList<Feld> getAlleFelder() {
-        return alleFelder;
-        
+        return alleFelder;  
     }
 
     public List<Feld> getUnkaufbareFelder() {
@@ -41,18 +47,30 @@ public class Spielfeld extends JPanel {
     public Feld feldAmOrt(int ort) {
         return alleFelder.get(ort);
     }
+    
+    public static Feld getFeld(int index) {
+        return alleFelder.get(index);
+    }
 
+    public void addSpieler(Spielfigur spieler) {
+        spielerListe.add(spieler);
+    }
 
-    public Spielfeld(int xKoord, int yKoord, int breite, int hoehe) {
-        setBorder(new LineBorder(new Color(0, 0, 0)));
-        setBounds(xKoord, yKoord, breite, hoehe);
-        this.setLayout(null);
-        initialisierungModule();
+    public boolean isPasch(int würfel1, int würfel2) {
+        return würfel1 == würfel2;
     }
     
-    
+    public boolean isCanRollDice() {
+        return canRollDice;
+    }
 
+    public void setCanRollDice(boolean canRollDice) {
+        this.canRollDice = canRollDice;
+    }
+  
+    
     private void initialisierungModule() {
+    	
         String[] modulNamen = { "Start", "Digital Learning/Englisch", "Gemeinschaftsfeld", "Präperationstechnik",
                 "O-Woche", "Werder Straße", "Allg. Biologie/ Bionik", "Mathe/ Informatik",
                 "Statistische Datenanalyse", "Chemie/ Physik", "Nachprüfung/ Klausureinsicht", "Spezielle Biologie",
@@ -63,13 +81,15 @@ public class Spielfeld extends JPanel {
                 "Prüfung nicht bestanden", "Entwicklungsprojekt 'Bionik'", "Organisationsbionik/ BWL",
                 "Gemeinschaftsfeld", "Optimierungsverfahren", "Bionik Innovations-Centrum", "Ereignisfeld",
                 "Bachelor Thesis", "Bachelor Abschluss-Party", "Bachelorabschluss" };
+        
         int[] preis = { 0, 60, 0, 60, 0, 200, 100, 100, 100, 120, 0, 140, 150, 140, 160, 200, 180, 0, 180, 200, 0, 220, 0,
                 220, 240, 200, 260, 260, 150, 280, 0, 300, 300, 0, 320, 200, 0, 350, 0, 400 };
+        
         int[] miete = { 0, 2, 0, 4, 0, 25, 6, 6, 6, 8, 0, 10, 15, 10, 12, 25, 14, 0, 14, 16, 0, 18, 0, 18, 20, 25, 22, 22,
                 15, 24, 0, 26, 26, 0, 28, 25, 0, 35, 0, 50 };
+        
         Color[] feldFarben = new Color[] {
         		Color.WHITE, Color.MAGENTA, Color.WHITE, Color.MAGENTA, Color.WHITE, Color.LIGHT_GRAY,Color.CYAN, Color.CYAN, Color.CYAN, Color.CYAN, Color.WHITE, Color.PINK, Color.GRAY, Color.PINK, Color.PINK, Color.LIGHT_GRAY, Color.ORANGE, Color.WHITE, Color.ORANGE, Color.ORANGE, Color.WHITE, Color.RED, Color.WHITE, Color.RED, Color.RED, Color.LIGHT_GRAY, Color.YELLOW, Color.YELLOW, Color.GRAY, Color.YELLOW, Color.WHITE, Color.GREEN, Color.GREEN, Color.WHITE, Color.GREEN, Color.LIGHT_GRAY, Color.WHITE, Color.BLUE, Color.WHITE, Color.BLUE};
-        
 
         for (int i = 0; i < modulNamen.length; i++) {
             int x, y;
@@ -78,34 +98,42 @@ public class Spielfeld extends JPanel {
             if (i == 0) {
                 x = 400;
                 y = 703;
-            } else if (0 < i && i < 10) {
+            } 
+            else if (0 < i && i < 10) {
                 x = 400;
                 y = 703 - 70 * i;
-            } else if (i == 10) {
+            } 
+            else if (i == 10) {
                 x = 400;
                 y = 0;
                 rotation = 180;
-            } else if (10 < i && i < 20) {
+            } 
+            else if (10 < i && i < 20) {
                 x = 400 + 70 * (i - 10);
                 y = 0;
                 rotation = 180;
-            } else if (i == 20) {
+            } 
+            else if (i == 20) {
                 x = 1103;
                 y = 2;
                 rotation = -90;
-            } else if (20 < i && i < 30) {
+            } 
+            else if (20 < i && i < 30) {
                 x = 1103;
                 y = 3 + 70 * (i - 20);
                 rotation = -90;
-            } else if (i == 30) {
+            } 
+            else if (i == 30) {
                 x = 1104;
                 y = 705;
                 rotation = 0;
-            } else if (30 < i && i < 40) {
+            } 
+            else if (30 < i && i < 40) {
                 x = 1103 - 70 * (i - 30);
                 y = 703;
                 rotation = 0;
-            } else {
+            } 
+            else {
                 continue;
             }
             Color feldColor = feldFarben[i];
@@ -121,24 +149,11 @@ public class Spielfeld extends JPanel {
         }
     }
 
-    public static int getAnzahlFelder() {
-        return anzahlFelder;
-    }
 
-    public static Feld getFeld(int index) {
-        return alleFelder.get(index);
-    }
-
-    public void addSpieler(Spielfigur spieler) {
-        spielerListe.add(spieler);
-    }
-
-    public boolean isPasch(int würfel1, int würfel2) {
-        return würfel1 == würfel2;
-    }
     
 
     public void würfelnUndBewegen(Spielfigur spieler) {
+    	
         if (spieler.istPleite()) {
             System.out.println(spieler.getName() + " ist pleite und kann nicht mehr studieren.");
             return;
@@ -158,7 +173,8 @@ public class Spielfeld extends JPanel {
                     System.out.println(spieler.getName() + " hat die Nachprüfung bestanden.");
                     spieler.setInNachpruefung(false);
                     spieler.setNachpruefungVersuche(0);
-                } else {
+                } 
+                else {
                     spieler.decreaseNachpruefungVersuche();
                     System.out.println(spieler.getName() + " hat die Nachprüfung nicht bestanden und hat noch " + spieler.getNachpruefungVersuche() + " Versuche übrig.");
                     if (spieler.getNachpruefungVersuche() == 0) {
@@ -170,7 +186,8 @@ public class Spielfeld extends JPanel {
                 canRollDice = true; // Nächster Spieler kann würfeln
                 return;
             }
-        } else {
+        } 
+        else {
             Würfel würfel = new Würfel();
             würfel.würfel();
             int augensumme = würfel.getAugensumme();
@@ -255,40 +272,24 @@ public class Spielfeld extends JPanel {
                         spieler.setIntelligenz(spieler.getIntelligenz() - spieler.getAktuellesFeld().getPreis());
                         spieler.getAktuellesFeld().setBesitzer(spieler);
                         System.out.println(spieler.getName() + " hat " + spieler.getAktuellesFeld().getName() + " betanden und verliert " + spieler.getAktuellesFeld().getPreis() + " Intelligenz.\n");
-                    } else {
+                    } 
+                    else {
                         System.out.println(spieler.getName() + " hat nicht genug Intelligenz, um das Modul zu bestehen.");
                     }
-                } else {
-                    System.out.println(spieler.getName() + " hat sich gegen den Kauf von " + spieler.getAktuellesFeld().getName() + " entschieden.\n");
+                } 
+                else {
+                    System.out.println(spieler.getName() + " hat sich gegen das belegen von " + spieler.getAktuellesFeld().getName() + " entschieden.\n");
                 }
                 canRollDice = true; // Nächster Spieler kann würfeln
                 return;
-            } else if (spieler.getAktuellesFeld().getBesitzer() != null && spieler.getAktuellesFeld().getBesitzer() != spieler) {
-                mieteZahlen(spieler.getAktuellesFeld(), spieler);
+            } 
+            else if (spieler.getAktuellesFeld().getBesitzer() != null && spieler.getAktuellesFeld().getBesitzer() != spieler) {
+            	spieler.mieteZahlen(spieler.getAktuellesFeld());
             }
-
             canRollDice = true; // Nächster Spieler kann würfeln
         }
-        
-    }
-    
-
-    public void mieteZahlen(Feld feld, Spielfigur spieler) {
-        Spieler besitzer = feld.getBesitzer();
-        if (besitzer != null && !besitzer.equals(spieler)) {
-            int miete = feld.getMiete();
-            if (spieler.getIntelligenz() >= miete) {
-                spieler.setIntelligenz(spieler.getIntelligenz() - miete);
-                besitzer.setIntelligenz(besitzer.getIntelligenz() + miete);
-                System.out.println(spieler.getName() + " hat " + miete + " Intelligenz an " + besitzer.getName() + " für Nachhilfe in dem Modul " + feld.getName() + " gezahlt.");
-            } else {
-                System.out.println(spieler.getName() + " hat nicht genug Intelligenz, um die Nachhilfe zu zahlen.");
-                moduleÜbertragen(spieler, (Spielfigur) besitzer);
-                spieler.spielerPleite(spieler);
-                spielerListe.remove(spieler);
-            }
-        }
-    }
+   }
+   
     
     public void zugBeenden() {
         Spielfigur spieler = spielerListe.get(currentPlayerIndex);
@@ -299,30 +300,10 @@ public class Spielfeld extends JPanel {
         }
         canRollDice = true;
     }
-    public Feld feldAmOrt2(int index) {
-        if (index < 0 || index >= alleFelder.size()) {
-            throw new IllegalArgumentException("Ungültiger Index: " + index);
-        }
-        return alleFelder.get(index);
-    }
-
-    public boolean isCanRollDice() {
-        return canRollDice;
-    }
-
-    public void setCanRollDice(boolean canRollDice) {
-        this.canRollDice = canRollDice;
-    }
     
     
-    public void moduleÜbertragen(Spielfigur vonSpieler, Spielfigur zuSpieler) {
-        for (Feld feld : getAlleFelder()) {
-            if (feld.getBesitzer() != null && feld.getBesitzer().equals(vonSpieler)) {
-                feld.setBesitzer(zuSpieler);
-                System.out.println("Das Modul '" + feld.getName() + "' wurde von " + vonSpieler.getName() + " an " + zuSpieler.getName() + " übertragen.");
-            }
-        }
-    }
+    
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("Spielbrett Übersicht");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
