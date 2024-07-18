4fd4e2216375f3775d5a4889d0fb45c337d5cfbd
Theresa <168730143+TheresaRiess@users.noreply.github.com>
2024-07-18 21:06:46 +0200

    Update Währung.java

--- Bionopoly/Währung.java
+++ Bionopoly/Währung.java
@@ -11,6 +11,66 @@ public class Währung {
 
     
     public Währung() {
+    	this.startgeld = 1500;
+    }
+    
+    public Währung(Spielfeld[][] feld, Spielfigur[] spieler, int überStart) { //Konstrukter mit den Parametern Spielfeld, Spielfigur,ÜberStart und Transaktion
+    	
+    	//Deklarierung einiger Variablen
+        this.feld = feld;
+        this.spieler = spieler;
+        this.überStart = überStart;
+    }
+    
+    //Getter und Setter für verschiedene Methoden
+    public void setStartgeld(int startgeld) {
+        this.startgeld = startgeld;
+    }
+ 
+    public Spielfeld[][] getFeld() { 
+        return feld;
+    }
+
+    public void setFeld(Spielfeld[][] feld) {
+        this.feld = feld;
+    }
+
+    public Spielfigur[] getSpieler() {
+		return spieler;
+    }
+
+    public void setSpieler(Spielfigur[] spieler) {
+        this.spieler = spieler;
+    }
+
+    public int getStartgeld() {
+        return startgeld;
+    }
+
+    public int getÜberStart() {
+        return überStart;
+    }
+
+    public void setÜberStart(int überStart) {
+        this.überStart = überStart;
+    }
+
+    public void überStart(Spielfigur spieler) { //Methode die den Spielern 200 Intelligenzpunkte gibt wenn sie über Start ziehen
+        spieler.setIntelligenz(spieler.getIntelligenz() + 200); //Spieler erhält 200 Intelligenz wenn er über das Startfeld zieht
+    }
+}package bionopoly;
+
+public class Währung {
+    
+	//Intitialisierung verschiedener Variablen etc. 
+    private Spielfeld[][] feld;
+    private Spielfigur[] spieler; 
+    
+    private int startgeld;
+    private int überStart;
+
+    
+    public Währung() {
     	
     }
     
