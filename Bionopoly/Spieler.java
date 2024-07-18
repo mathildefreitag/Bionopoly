e210e4491395d815613d697c71ef161f99423abc
mathildefreitag <168734519+mathildefreitag@users.noreply.github.com>
2024-07-18 18:06:07 +0200

    Update Spieler.java

--- Bionopoly/Spieler.java
+++ Bionopoly/Spieler.java
@@ -13,7 +13,7 @@ public class Spieler {
     private boolean pleite;
     
     private List<Feld> besitz;
-    private ArrayList<Spielfigur> spielerListe = new ArrayList<>();
+    private ArrayList<Spielfigur> spielerListe = new ArrayList<>(); //Initialisierung der Arrayliste für die Zuordnung von Spielfiguren zu Spielern
    
     private Spielfeld spielfeld;
 
@@ -58,50 +58,53 @@ public class Spieler {
         return spielfeld;
     }
     
+    //Methode für den Fall, dass ein Spieler pleite ist (keine Intelligenz)
     public void spielerPleite(Spieler spieler) {
         System.out.println(spieler.getName() + " hat all seine Intelligenz verloren und muss das Bionik Studium schmeißen.");
         spieler.setPleite(true);
     }
 
-
+    //Methode zum Kaufen/Bestehen eines Moduls
     public void feldKaufen(Feld feld) {
         if (!hatBesitzer(feld) && !spielfeld.getUnkaufbareFelder().contains(feld)) {
             System.out.println("Möchtest du " + feld.getPreis() + " Intelligenz in das Modul " + feld.getName() + " investieren? (Ja/Nein)");
             Scanner scanner = new Scanner(System.in);
             String antwort = scanner.nextLine();
 
-            if (antwort.equalsIgnoreCase("ja")) {
-                if (intelligenz >= feld.getPreis()) {
-                    setIntelligenz(getIntelligenz() - feld.getPreis());
-                    feld.setBesitzer(this);
-                    besitz.add(feld);
-                    System.out.println(name + " hat " + feld.getName() + " für " + feld.getPreis() + " Intelligenz erhalten.");
+            if (antwort.equalsIgnoreCase("ja")) { //wenn sich der Spieler entscheidet das Modul zu kaufen bzw. zu belegen 
+                if (intelligenz >= feld.getPreis()) { //wird geprüft ob genug Intelligenz vorhanden ist
+                    setIntelligenz(getIntelligenz() - feld.getPreis()); //Preis des Moduls wird von der Intelligenz abgezogen
+                    feld.setBesitzer(this); //Feld bekommt einen Besitzer (der Spieler der es gekauft hat)
+                    besitz.add(feld); //Besitzer wird dem Feld hinzugefügt
+                    System.out.println(name + " hat " + feld.getName() + " für " + feld.getPreis() + " Intelligenz erhalten."); //Ausgabe (in Konsole)
                 } 
                 else {
-                    System.out.println(name + " hat nicht genug Intelligenz, um " + feld.getName() + " zu belegen.");
+                    System.out.println(name + " hat nicht genug Intelligenz, um " + feld.getName() + " zu belegen."); //Ausgabe falls nicht genug Intelligenz vorhanden ist 
                 }
             } 
             else {
-                System.out.println("Du hast entschieden, " + feld.getName() + " zu schieben.");
+                System.out.println("Du hast entschieden, " + feld.getName() + " zu schieben."); //für den Fall, dass der Spieler das Modul nicht belegen/kaufen möchte
             }
         } 
         else {
-            System.out.println(feld.getName() + " kann nicht erhalten werden.");
+            System.out.println(feld.getName() + " kann nicht erhalten werden."); //Falls ein Fehler im Verkaufsvorgang/ bei der Mieteinnahme auftritt
         }
     }
 
+    //Methode zum Zahlen der Nachhilfe (Miete)
     public void mieteZahlen(Feld feld) {
         Spieler besitzer = feld.getBesitzer();
-        if (besitzer != null && !besitzer.equals(this)) {
-            int miete = feld.getMiete();
-            if (this.getIntelligenz() >= miete) {
-                this.setIntelligenz(this.getIntelligenz() - miete);
-                besitzer.setIntelligenz(besitzer.getIntelligenz() + miete);
-                System.out.println(this.getName() + " hat " + miete + " Intelligenz an " + besitzer.getName() + " für Nachhilfe in dem Modul " + feld.getName() + " gezahlt.");
-            } else {
-                System.out.println(this.getName() + " hat nicht genug Intelligenz, um die Nachhilfe zu zahlen.");
-                this.spielerPleite(this);
-                spielerListe.remove(this);
+        if (besitzer != null && !besitzer.equals(this)) { //Wenn das Feld bereits einen Besitzer hat, der nicht der aktuelle Spieler ist, wird diese Schleife gestartet. 
+            int miete = feld.getMiete(); //die Miete entspricht der für das Feld festgelegten Miete
+            if (this.getIntelligenz() >= miete) { //Prüfen ob genug Intelligenz vorhanden ist um Nachhilfestunden zu nehmen
+                this.setIntelligenz(this.getIntelligenz() - miete); //Intelligenzpunkte in Höhe der Miete werden vom aktuellen Spieler abgezogen
+                besitzer.setIntelligenz(besitzer.getIntelligenz() + miete); // Der Besitzer des Feldes bekommt die Intelligenzpunkte für die Nachhilfe
+                System.out.println(this.getName() + " hat " + miete + " Intelligenz an " + besitzer.getName() + " für Nachhilfe in dem Modul " + feld.getName() + " gezahlt."); //Ausgabe für die Ausführung der Aktion
+            } 
+            else {
+                System.out.println(this.getName() + " hat nicht genug Intelligenz, um die Nachhilfe zu zahlen."); // wenn nicht genug Intelligenz vorhanden um Miete/Nachhilfe zu zahlen
+                this.spielerPleite(this); //aktueller Spieler ist pleite
+                spielerListe.remove(this); //Der aktueller Spieler wird entfernt, weil er pleite ist: Der Spieler muss das Studium abbrechen, weil er nicht genug Intelligenz hat.
             }
         }
     }
